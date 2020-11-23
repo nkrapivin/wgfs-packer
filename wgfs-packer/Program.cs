bfde2338b2cca64eb47e9282b9a4ed6b38ce10ba
Nikita Krapivin <alienoom@yandex.ru>
2020-11-23 16:57:44 +0500

    Initial commit

---
+++ wgfs-packer/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace wgfs_packer
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("WGFS-Packer - Weird Game File System Packer v1.0");
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: <this program> 'directory to pack' 'string table'");
+                Console.ReadKey(true);
+                return 0;
+            }
+
+            string dir = args[0];
+            string jsonpath = args[1];
+            var strm = new FileStream("data.dat", FileMode.Create, FileAccess.Write, FileShare.Read);
+            Console.WriteLine("Packing files...");
+
+            var fs = new Assets();
+            foreach (var file in Directory.EnumerateFiles(dir, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                Console.WriteLine("Adding " + file);
+
+                fs.AddFile(new File {
+                    name = Path.GetFileName(file),
+                    data = System.IO.File.ReadAllBytes(file)
+                });
+            }
+
+            var table = JObject.Parse(System.IO.File.ReadAllText(jsonpath));
+            Console.WriteLine("Packing strings...");
+            foreach (var item in table)
+            {
+                string v = item.Value.Value<string>();
+                fs.AddString(item.Key, v);
+                Console.WriteLine($"{item.Key}|{v}");
+            }
+
+            Console.WriteLine("Serializing...");
+            fs.Serialize(strm);
+            strm.Flush(true);
+            strm.Dispose();
+
+            Console.WriteLine("Done!");
+            Console.ReadKey(true);
+
+            return 0;
+        }
+    }
+}
---
+++ wgfs-packer/WGFS.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace wgfs_packer
+{
+    public static class Utils
+    {
+        public static void WriteASCII(BinaryWriter bw, string value)
+        {
+            byte[] nameAsASCII = Encoding.ASCII.GetBytes(value);
+            bw.Write(nameAsASCII);
+            bw.Write((byte)0x00); // null terminate the string.
+        }
+    }
+
+    public struct File
+    {
+        public string name;
+        public byte[] data;
+
+        public void Serialize(BinaryWriter w)
+        {
+            Utils.WriteASCII(w, name);
+            w.Write(data.Length);
+            w.Write(data);
+        }
+    }
+
+    public class Assets
+    {
+        private List<File> Files { get; set; } = new List<File>();
+
+        private Dictionary<string, string> StringTable { get; set; } = new Dictionary<string, string>();
+
+        private const int WGFS_HEADER = 1397114711;
+        private const int WEND_HEADER = 1145980247;
+        private const int FILE_HEADER = 1162627398;
+        private const int STRG_HEADER = 1196577875;
+        private const int WGFS_VERSION = 1;
+
+        private void SerializeFiles(BinaryWriter bw)
+        {
+            bw.Write(FILE_HEADER); // 'FILE'
+            bw.Write(Files.Count);
+            foreach (var file in Files)
+            {
+                file.Serialize(bw);
+            }
+        }
+
+        private void SerializeStrings(BinaryWriter bw)
+        {
+            bw.Write(STRG_HEADER); // 'STRG'
+            bw.Write(StringTable.Count);
+            foreach (var kvp in StringTable)
+            {
+                Utils.WriteASCII(bw, kvp.Key);
+                Utils.WriteASCII(bw, kvp.Value);
+            }
+        }
+
+        private void SerializeAll(BinaryWriter bw)
+        {
+            bw.Write(WGFS_HEADER); // 'WGFS' header
+            bw.Write(WGFS_VERSION);
+            this.SerializeFiles(bw);
+            this.SerializeStrings(bw);
+            bw.Write(WEND_HEADER); // 'WEND'
+            // TODO: more stuff here?
+        }
+
+        public void Serialize(Stream stream)
+        {
+            var bw = new BinaryWriter(stream);
+            SerializeAll(bw);
+            //bw.Dispose();
+        }
+
+        public void AddFile(File f)
+        {
+            Files.Add(f);
+        }
+
+        public void AddString(string key, string value)
+        {
+            StringTable.Add(key, value);
+        }
+    }
+}
